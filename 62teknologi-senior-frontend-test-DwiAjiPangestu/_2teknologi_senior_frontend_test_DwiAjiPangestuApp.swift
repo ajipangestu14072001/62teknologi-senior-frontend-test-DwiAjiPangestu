8ecf087b3ce6bb4913745d0b1ac559fdc00a5fde
aji <ajipangestu14072001@gmail.com>
2023-06-20 09:50:52 +0700

    Initial Commit

---
+++ 62teknologi-senior-frontend-test-DwiAjiPangestu/_2teknologi_senior_frontend_test_DwiAjiPangestuApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2teknologi_senior_frontend_test_DwiAjiPangestuApp.swift
+//  62teknologi-senior-frontend-test-DwiAjiPangestu
+//
+//  Created by Dwi Aji Pangestu on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct _2teknologi_senior_frontend_test_DwiAjiPangestuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
